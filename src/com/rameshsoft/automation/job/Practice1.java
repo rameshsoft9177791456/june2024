b3bbdd4cd147ed8e47f54a165625edb38f0d51ab
rameshsoft9177791456 <rameshsoft.9177791456@gmail.com>
2024-07-03 21:13:16 +0530

    vennela Practice1 file is added

---
+++ src/com/rameshsoft/automation/job/Practice1.java
@@ -0,0 +1,13 @@
+package com.rameshsoft.automation.job;
+
+public class Practice {
+
+	public void practice() {
+		System.out.println("Practice");
+System.out.println("Practice = Job");
+		System.out.println("Do hardwork");
+
+	}
+	
+	
+}
